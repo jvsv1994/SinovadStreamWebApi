8c791ba19feaebe0c5e88c00841968b454aa733b
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-08-21 20:20:42 -0500

    JS - Enhancements

--- SinovadDemoWebApi/Service/SinovadDemoWebApi/CustomHub/MediaServerHub.cs
+++ SinovadDemoWebApi/Service/SinovadDemoWebApi/CustomHub/MediaServerHub.cs
@@ -4,35 +4,41 @@ namespace SinovadDemoWebApi.CustomHub
 {
     public class MediaServerHub: Hub
     {
-        public async Task UpdateItemsByMediaServer(string mediaServerGuid)
+
+        // for web client
+
+        public async Task UpdateMediaServers(string userGuid)
         {
-            await Clients.Group(mediaServerGuid).SendAsync("UpdateItemsByMediaServer");
+            await Clients.Group(userGuid).SendAsync("UpdateMediaServers");
         }
-        public async Task UpdateItemsByLibrary(string mediaServerGuid,string libraryGuid)
+        public async Task UpdateLibrariesByMediaServer(string userGuid, string mediaServerGuid)
         {
-            await Clients.Group(mediaServerGuid).SendAsync("UpdateItemsByLibrary", libraryGuid);
+            await Clients.Group(userGuid).SendAsync("UpdateLibrariesByMediaServer", mediaServerGuid);
         }
-        public async Task UpdateLibraries(string mediaServerGuid)
+        public async Task UpdateItemsByMediaServer(string userGuid, string mediaServerGuid)
         {
-            await Clients.Group(mediaServerGuid).SendAsync("UpdateLibraries");
+            await Clients.Group(userGuid).SendAsync("UpdateItemsByMediaServer", mediaServerGuid);
+        }
+        public async Task UpdateItemsByMediaServerAndLibrary(string userGuid, string mediaServerGuid, string libraryGuid)
+        {
+            await Clients.Group(userGuid).SendAsync("UpdateItemsByMediaServerAndLibrary", mediaServerGuid, libraryGuid);
+        }
+
+        public async Task AddConnectionToUserClientsGroup(string userGuid)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId,userGuid);
+        } 
+
+        // for media server client
+
+        public async Task AddConnectionToMediaServerClientsGroup(string mediaServerGuid)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, mediaServerGuid);
         }
         public async Task UpdateTranscoderSettings(string mediaServerGuid)
         {
             await Clients.Group(mediaServerGuid).SendAsync("UpdateTranscoderSettings");
         }
-        public async Task UpdateMediaServers(string userGuid)
-        {
-            await Clients.Group(userGuid).SendAsync("UpdateMediaServers");
-        }
-        public async Task AddConnectionToUserClientsGroup(string userGuid)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId,userGuid);
-        } 
-        public async Task AddConnectionToMediaServerClientsGroup(string mediaServerGuid)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, mediaServerGuid);
-        }
-
     }
 
 }
