d9dbd3a8ff19ff03e91434430ffb1cc3e8deb371
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-09-21 14:25:04 -0500

    JS - Enhancements

---
+++ SinovadDemoWebApi/Persistence/SinovadDemo.Persistence/Migrations/20230921192419_210920233.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinovadDemo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class _210920233 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Catalog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(1000)", unicode: false, maxLength: 1000, nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NewId()"),
+                    Deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Catalog__3214EC275C8D2947", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogDetail",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    CatalogId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(1000)", unicode: false, maxLength: 1000, nullable: true),
+                    TextValue = table.Column<string>(type: "varchar(1000)", unicode: false, maxLength: 1000, nullable: true),
+                    NumberValue = table.Column<int>(type: "int", nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NewId()"),
+                    Deleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__CatalogD__5C6FE914EF292CEC", x => new { x.CatalogId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_CatalogDetail_Catalog_ID",
+                        column: x => x.CatalogId,
+                        principalTable: "Catalog",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Guid",
+                value: new Guid("f1b339d0-191e-4900-b5fb-592b3f4253c8"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Guid",
+                value: new Guid("b971c9f9-e8c2-4afb-98a6-6d4ad33c0568"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Guid",
+                value: new Guid("b824e73a-68ba-4dbf-9ffa-970b0abf9f69"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Guid",
+                value: new Guid("f02f0544-4295-4aa9-aba9-d128ab2d7ff5"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Guid",
+                value: new Guid("54eabf00-da04-40e3-a83f-ff8fac651ce4"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Guid",
+                value: new Guid("c5a6828c-49b7-4ed9-9452-a670619b4d71"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Guid",
+                value: new Guid("f4773027-9168-4b92-b65f-3c05f24f7283"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Guid",
+                value: new Guid("f678cf8c-d870-45cb-8135-91ec9665dcc9"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Guid",
+                value: new Guid("593c1722-da9f-407b-9de6-082fabd2a890"));
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Guid",
+                value: new Guid("6b7a271f-1d2f-4b4f-b864-885d1df20d22"));
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Guid",
+                value: new Guid("765476b1-fc4b-407e-914f-5a447b0fa881"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CatalogDetail");
+
+            migrationBuilder.DropTable(
+                name: "Catalog");
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Guid",
+                value: new Guid("3d93aaaf-90e7-4899-93d6-48ca8b8afa83"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Guid",
+                value: new Guid("46f273ca-6b6e-4e81-96f6-1ec4baa41df3"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Guid",
+                value: new Guid("c82531ab-1856-4105-8112-c68acd068cff"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Guid",
+                value: new Guid("6e794296-c5f2-43a5-b26f-1146ff3ba52a"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Guid",
+                value: new Guid("13aa7957-6b2e-47f2-ae43-f34bf63cb914"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Guid",
+                value: new Guid("ea8740bb-c921-4b99-916d-e8e4f0acda5c"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Guid",
+                value: new Guid("e31f9b66-b740-4047-8a96-be1f3d0592c1"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Guid",
+                value: new Guid("6f5bf2bb-52ac-43c3-b71f-4e6595145808"));
+
+            migrationBuilder.UpdateData(
+                table: "Menu",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Guid",
+                value: new Guid("fe5c7c05-882e-4672-b6d1-57f26e1e3bd9"));
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Guid",
+                value: new Guid("6c2b12a3-4c08-4f5d-a86d-c1e1a63e2b9a"));
+
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Guid",
+                value: new Guid("53652724-fe9e-4e95-b0f0-f349920432c6"));
+        }
+    }
+}
